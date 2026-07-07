9dcb1affdaf0ae3aaf25a8f62607d7e23cc55b20
zohaibhassan3 <zohaibenroute3@gmail.com>
2026-07-07 12:00:29 +0500

    files changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
   },
+  chromeWebSecurity: false,
 });
---
+++ cypress/e2e/CMS.CreateAccount.cy.js
@@ -0,0 +1,44 @@
+
+describe('Create Account',()=>{
+  const randomAccountname = Array.from({length: 6}, () => String.fromCharCode(65 + Math.floor(Math.random() * 26))).join('')
+       const bankNames = ['Emirates NBD', 'Abu Dhabi Commercial Bank', 'Dubai Islamic Bank', 'First Abu Dhabi Bank', 'UBL Bank']
+const randomBank = bankNames[Math.floor(Math.random() * bankNames.length)]
+       const randomIBAN = 'AE' + Math.floor(Math.random() * 90 + 10) + Math.floor(Math.random() * 9000000000000000 + 1000000000000000)
+       const randomAccountNumber = Math.floor(Math.random() * 9000000000 + 1000000000).toString()
+    it('Create account( type = bank )',()=>{
+        
+ cy.visit('http://dev2.sianty.com/#/login')
+ 
+cy.get('[name="email"]').type('zohaib@email.com')
+cy.get('[name="password"]').type('zohaib12')
+cy.get(':nth-child(5) > .MuiButtonBase-root').click() // login done
+   cy.get('[data-key="business_setup"] > .MuiButtonBase-root').trigger('mouseover')
+cy.contains('Accounts').click()
+       cy.get('.flex > div > .MuiButtonBase-root').click() 
+       // Create form opened
+     
+cy.get('[name="accountTitle"]').type(randomAccountname)
+cy.get('[name="bankName"]').type(randomBank)
+cy.get('[name="accountNumber"]').type(randomAccountNumber)
+cy.get('[name="iban"]').type(randomIBAN)
+cy.get('[name="openingBalance"]').type('5000')
+cy.get('.formSubmitBtn > .MuiButtonBase-root').click({force: true})
+})
+it.only('Create account( type = bank )',()=>{
+        
+ cy.visit('http://dev2.sianty.com/#/login')
+ 
+cy.get('[name="email"]').type('zohaib@email.com')
+cy.get('[name="password"]').type('zohaib12')
+cy.get(':nth-child(5) > .MuiButtonBase-root').click() // login done
+   cy.get('[data-key="business_setup"] > .MuiButtonBase-root').trigger('mouseover')
+cy.contains('Accounts').click()
+       cy.get('.flex > div > .MuiButtonBase-root').click() 
+       // Create form opened
+       cy.get('[name="type"]').parent().click()
+cy.contains('Cash').click()
+cy.get('[name="accountTitle"]').type(randomAccountname)
+cy.get('[name="openingBalance"]').type('2000')
+cy.get('.formSubmitBtn > .MuiButtonBase-root').click({force: true})
+})
+})
